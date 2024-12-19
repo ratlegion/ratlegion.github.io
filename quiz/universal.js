d8798c54ff2f90ccf1c2be2c2b0c45bc7f552051
ratlegion <165434018+ratlegion@users.noreply.github.com>
2024-12-19 12:35:44 -0800

    trying to fight cors

--- quiz/universal.js
+++ quiz/universal.js
@@ -33,28 +33,49 @@
 // updateStyle()
 
 // Set the style to the style of a given element
-function setStyle(element){
+function setStyle(element) {
     let elementStyle = element.parentNode.getAttribute("data-style");
-    console.log("Set style to "+elementStyle)
+    console.log("Set style to " + elementStyle)
     applyStyle(elementStyle);
 }
 
-function applyStyle(style){
+function applyStyle(style) {
 
     // If this is editor mode and we have a selected themes menu, remove the current selected theme and set it the the applied style
     const selectedTheme = document.getElementById('selected-theme');
     if (selectedTheme) {
         selectedTheme.removeAttribute('id');
 
-        let newStyle = document.querySelector('[data-style='+style+']');
-        newStyle.id = "selected-theme"
+        let newStyle = document.querySelector('[data-style=' + style + ']');
+        newStyle.id = "selected-theme";
     }
 
     document.getElementById('styletheme').href = "themes/" + style + "/style.css";
+
+    addStyleExtension(style);
 }
 
-function addStyleExtension(style){
-    
+function addStyleExtension(style) {
+    //<link rel="stylesheet" class="extends-styles" id="extends-styles-1" href="">
+
+    let currentExtendsStyle = Array.from(document.getElementsByClassName('extends-styles'));
+    currentExtendsStyle.forEach(element => {
+        element.remove();
+    });
+
+
+    // cors is going to be real fun.
+    fetch('/themes/natural/settings.json')
+        .then(response => response.json())  // Parse the JSON response
+        .then(data => {
+            console.log(data);  // Use the data from the JSON file
+        })
+        .catch(error => {
+            console.error('Error loading JSON:', error);
+        });
+
+
+
 }
 
 const queryString = window.location.search;
